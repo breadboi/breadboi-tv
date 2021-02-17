364c479b06e00023dfcc5f916b47f159e0df1ec7
Brett Carney <brett.carney1273@gmail.com>
2021-02-17 05:17:52 -0600

    22 Added pre-existing modules

--- src/jsx/schedule.jsx
+++ src/jsx/schedule.jsx
@@ -62,10 +62,6 @@ class Schedule extends React.Component {
         return (
             <div className="schedule-section">
                 <div className="container text-center">
-                    <div className="fa-stack center-block medium-icon-red fa-5x">
-                        <i className="fa fa-circle fa-stack-2x"></i> <i className="fa fa-clock-o fa-stack-1x fa-inverse"></i>
-                    </div>
-                    <h1 className="text-gdq-red extra-spacing">Bread boi's 24hr Stream Schedule</h1>
                     <h4 className="text-gdq-black well ">All start times below are converted to your local time<span id="offset-detected"> ({moment.tz.guess()})</span>.</h4>
                 </div>
 
